deae72aa4b9363956efd1b6c4544782c5dd2405f
himanshk96 <kriplani@usc.edu>
2020-01-07 18:18:51 -0800

    Added dropzone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+
+function MyDropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <MyDropzone />
           Learn React
-        </a>
+        
       </header>
     </div>
   );
